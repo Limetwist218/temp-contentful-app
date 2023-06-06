f65a17b35c693afb6fdc68c9814bbdc9f12be926
Adeeb Rohani2 <adeebrohani2@Adeebs-MBP.tampabay.rr.com>
2023-06-06 14:54:23 -0400

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+            hic earum sequi dignissimos sapiente unde consequatur commodi, nihil
+            fugit voluptatem.
+          </p>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
